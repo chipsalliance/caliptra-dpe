a779caf8458a5ded1af3fa7314ac1cc63c1e1d92
Jordan Hand <jhand@google.com>
2023-02-17 16:40:57 -0800

    Make Context struct conform to the C ABI
    For impactless updates, it may be necessary to store TCI contexts in
    RAM to be usable by different firmware versions. Since Rust does not
    have a stable ABI, make the context structure conform to the C ABI.

--- dpe/src/commands.rs
+++ dpe/src/commands.rs
@@ -1,7 +1,9 @@
 use crate::profile;
 use crate::{DpeError, MAX_HANDLES};
 
-use core::option::Option;
+fn set_flag(field: &mut u32, mask: u32, value: bool) {
+    *field = if value { *field | mask } else { *field & !mask };
+}
 
 // ABI Command/Response structures
 
@@ -40,18 +42,59 @@ pub struct InitCtxResp {
     pub hdr: ResponseHdr,
 }
 
+#[repr(C, align(4))]
+#[derive(Default)]
+struct TciNodeData {
+    tci_type: u32,
+
+    // Bits
+    // 31: INTERNAL
+    // 30-0: Reserved. Must be zero
+    flags: u32,
+    tci_cumulative: [u8; profile::TCI_SIZE],
+    tci_current: [u8; profile::TCI_SIZE],
+}
+
+impl TciNodeData {
+    const INTERNAL_FLAG_MASK: u32 = 1 << 31;
+
+    const fn flag_is_internal(self) -> bool {
+        self.flags & Self::INTERNAL_FLAG_MASK != 0
+    }
+
+    fn set_flag_is_internal(&mut self, value: bool) {
+        set_flag(&mut self.flags, Self::INTERNAL_FLAG_MASK, value);
+    }
+
+    const fn new() -> TciNodeData {
+        TciNodeData {
+            tci_type: 0,
+            flags: 0,
+            tci_cumulative: [0; profile::TCI_SIZE],
+            tci_current: [0; profile::TCI_SIZE],
+        }
+    }
+}
+
+#[repr(C, align(4))]
 struct Context {
-    parent: Option<&'static Context>,
-    tci: [u8; profile::TCI_SIZE],
+    tci: TciNodeData,
+    // Bitmap of the node indices that are children of this node
+    children: u32,
+    // Index in DPE instance of the parent context. 0xFF if this node is the root
+    parent_idx: u8,
     simulation: bool,
+    is_active: bool,
 }
 
 impl Context {
     const fn new() -> Context {
         Context {
-            parent: None,
-            tci: [0; profile::TCI_SIZE],
+            tci: TciNodeData::new(),
+            children: 0,
+            parent_idx: 0xFF,
             simulation: false,
+            is_active: false,
         }
     }
 }
