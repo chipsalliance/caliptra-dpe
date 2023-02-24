4111c950c6be51f99ddb6638b619593d5a2d753a
Julien Cretin <cretin@google.com>
2023-02-24 11:24:45 +0100

    Use rustup directly instead of actions-rs

--- ci.sh
+++ ci.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -ex
 
+# TODO: Remove once we don't generate those warnings. They currently polute the
+# script output and prevents easily identifying more important warnings and
+# errors.
+export RUSTFLAGS='-A dead_code -A unused_variables'
+
 ( cd dpe
   cargo build
   # cargo build --no-default-features --features=dpe_profile_p384_sha384
