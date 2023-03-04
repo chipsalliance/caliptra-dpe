9d0d25b30fe25e7e7d4818dc504463f80bc8aac6
Zach Halvorsen <zhalvorsen@google.com>
2023-03-03 22:18:38 -0800

    Add verification tests to the CI scripts.
    Starts and stops a simulator while running the verification tests.

--- verification/verification_test.go
+++ verification/verification_test.go
@@ -1,20 +1,37 @@
 package verification
 
 import (
+	"flag"
+	"log"
 	"testing"
 )
 
+var sim_exe = flag.String("sim", "../simulator/target/debug/simulator", "path to simulator executable")
+
 func TestGetProfile(t *testing.T) {
+	simulator := DpeSimulator{}
+	defer simulator.Terminate()
+	err := simulator.Start(*sim_exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	client := DpeClient{transport: &SimulatorTransport{}, profile: DPE_PROFILE_P256_SHA256}
-	err, _ := client.GetProfile()
+	err, _ = client.GetProfile()
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestInitializeContext(t *testing.T) {
+	simulator := DpeSimulator{}
+	defer simulator.Terminate()
+	err := simulator.Start(*sim_exe)
+	if err != nil {
+		log.Fatal(err)
+	}
 	client := DpeClient{transport: &SimulatorTransport{}, profile: DPE_PROFILE_P256_SHA256}
-	err, _ := client.Initialize(NewInitCtxIsDefault())
+	err, _ = client.Initialize(NewInitCtxIsDefault())
 	if err != nil {
 		t.Fatal(err)
 	}
