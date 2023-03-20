046e5c8ec19c4a307c041072b959af6aaa5b5ebd
Zach Halvorsen <zhalvorsen@google.com>
2023-03-20 09:49:54 -0700

    Add more verification tests for InitializeContext.

--- verification/verification_test.go
+++ verification/verification_test.go
@@ -37,11 +37,11 @@ func TestGetProfile(t *testing.T) {
 func testGetProfile(s *DpeSimulator, t *testing.T) {
 	const MIN_TCI_NODES uint32 = 8
 	if s.HasPowerControl() {
-		defer s.PowerOff()
 		err := s.PowerOn()
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer s.PowerOff()
 	}
 	client := DpeClient{transport: s}
 
@@ -72,27 +72,103 @@ func testGetProfile(s *DpeSimulator, t *testing.T) {
 }
 
 func TestInitializeContext(t *testing.T) {
-	simulator := DpeSimulator{exe_path: *sim_exe}
-	defer simulator.PowerOff()
-	err := simulator.PowerOn()
-	if err != nil {
-		log.Fatal(err)
+	simulators := []DpeSimulator{
+		// No extra options.
+		{exe_path: *sim_exe},
+		// Supports simulation.
+		{exe_path: *sim_exe, supports: Support{Simulation: true}},
 	}
 
-	client := DpeClient{transport: &simulator}
-	err, respHdr, _ := client.GetProfile(DPE_SIMULATOR_AUTO_INIT_LOCALITY)
-	if err != nil {
-		t.Fatal(err)
+	for _, s := range simulators {
+		for _, l := range s.GetLocalities() {
+			testInitContext(&s, l, t)
+		}
 	}
-	if respHdr.Status != DPE_STATUS_SUCCESS {
-		t.Fatal("Unable to get profile.")
+}
+
+func testInitContext(s *DpeSimulator, locality uint32, t *testing.T) {
+	if s.HasPowerControl() {
+		err := s.PowerOn()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer s.PowerOff()
 	}
 
-	err, respHdr, _ = client.Initialize(DPE_SIMULATOR_AUTO_INIT_LOCALITY, NewInitCtxIsDefault())
+	client := DpeClient{transport: s}
+	// Need to set up the client completely by getting the profile.
+	err, respHdr, profile := client.GetProfile(locality)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if respHdr.Status != 0 {
-		t.Fatal("Failed to initialize default context.")
+		t.Fatal("Failed to get profile.")
+	}
+
+	// Try to create the default context if isn't done automatically.
+	if !client.transport.GetSupport().AutoInit {
+		err, respHdr, initCtxResp := client.Initialize(locality, NewInitCtxIsDefault())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if respHdr.Status != 0 {
+			t.Fatal("Failed to initialize default context.")
+		}
+		if initCtxResp.Handle != [16]byte{0} {
+			t.Fatal("Incorrect default context handle.")
+		}
+		defer client.DestroyContext(locality, NewDestroyCtx(initCtxResp.Handle, false))
+	}
+
+	// Try to initialize another default context.
+	err, respHdr, _ = client.Initialize(locality, NewInitCtxIsDefault())
+	if err == nil {
+		t.Fatal("The instance should return an error when trying to initialize another default context.")
+	}
+	if respHdr.Status != DPE_STATUS_ARGUMENT_NOT_SUPPORTED {
+		t.Fatalf("Incorrect error type. Should return %d, but returned %d", DPE_STATUS_ARGUMENT_NOT_SUPPORTED, respHdr.Status)
+	}
+
+	// Try to initialize a context that is neither default or simulation.
+	err, respHdr, _ = client.Initialize(locality, &InitCtxCmd{})
+	if err == nil {
+		t.Fatal("The instance should return an error when not default or simulation.")
+	}
+	if respHdr.Status != DPE_STATUS_INVALID_ARGUMENT {
+		t.Fatalf("Incorrect error type. Should return %d, but returned %d", DPE_STATUS_INVALID_ARGUMENT, respHdr.Status)
+	}
+
+	if !client.transport.GetSupport().Simulation {
+		// Try to initialize a simulation context when they aren't supported.
+		err, respHdr, _ := client.Initialize(locality, NewInitCtxIsSimulation())
+		if err == nil {
+			t.Fatal("The instance should return an error when trying to initialize another default context.")
+		}
+		if respHdr.Status != DPE_STATUS_ARGUMENT_NOT_SUPPORTED {
+			t.Fatalf("Incorrect error type. Should return %d, but returned %d", DPE_STATUS_ARGUMENT_NOT_SUPPORTED, respHdr.Status)
+		}
+	} else {
+		// Try to get the correct error for overflowing the contexts. Fill up the
+		// rest of the contexts (-1 for default).
+		for i := uint32(0); i < profile.MaxTciNodes-1; i++ {
+			err, respHdr, initCtxResp := client.Initialize(locality, NewInitCtxIsSimulation())
+			if err != nil || respHdr.Status != 0 {
+				t.Fatal("The instance should be able to create a simulation context.")
+			}
+			// Could prove difficult to prove it is a cryptographically secure random.
+			if initCtxResp.Handle == [16]byte{0} {
+				t.Fatal("Incorrect simulation context handle.")
+			}
+			defer client.DestroyContext(locality, NewDestroyCtx(initCtxResp.Handle, false))
+		}
+
+		// Now try to make one more than the max.
+		err, respHdr, _ := client.Initialize(locality, NewInitCtxIsSimulation())
+		if err == nil {
+			t.Fatal("Failed to report an error for too many contexts.")
+		}
+		if respHdr.Status != DPE_STATUS_MAX_TCIS {
+			t.Fatalf("Incorrect error type. Should return %d, but returned %d", DPE_STATUS_MAX_TCIS, respHdr.Status)
+		}
 	}
 }
