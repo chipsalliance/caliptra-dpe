6d9e0f36db84266cb97e5c000fec44d1b9549cd7
Sree Revoori <srrevoori@google.com>
2023-08-31 22:52:22 +0000

    Make GetCertificateChainCmd public

--- dpe/src/commands/mod.rs
+++ dpe/src/commands/mod.rs
@@ -6,7 +6,7 @@ Abstract:
 --*/
 pub use self::derive_child::{DeriveChildCmd, DeriveChildFlags};
 pub(crate) use self::destroy_context::DestroyCtxCmd;
-pub(crate) use self::get_certificate_chain::GetCertificateChainCmd;
+pub use self::get_certificate_chain::GetCertificateChainCmd;
 pub use self::initialize_context::InitCtxCmd;
 
 pub use self::certify_key::{CertifyKeyCmd, CertifyKeyFlags};
