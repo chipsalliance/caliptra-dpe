5ff80d2733d5739eb694486372eb8dca67c81e34
Sree Revoori <srrevoori@google.com>
2023-09-20 21:31:42 +0000

    Make destroy context clear destroyed context's parent's child bit
    Also add tests for destroy context.

--- dpe/src/commands/destroy_context.rs
+++ dpe/src/commands/destroy_context.rs
@@ -1,7 +1,7 @@
 // Licensed under the Apache-2.0 license.
 use super::CommandExecution;
 use crate::{
-    context::ContextHandle,
+    context::{Context, ContextHandle},
     dpe_instance::{flags_iter, DpeEnv, DpeInstance, DpeTypes},
     response::{DpeErrorCode, Response, ResponseHdr},
     MAX_HANDLES,
@@ -59,6 +59,10 @@ impl CommandExecution for DestroyCtxCmd {
                 return Err(DpeErrorCode::InternalError);
             }
             dpe.contexts[idx].destroy();
+            let parent = dpe.contexts[idx].parent_idx;
+            if parent != Context::ROOT_INDEX {
+                dpe.contexts[parent as usize].remove_child(idx)?;
+            }
         }
         Ok(Response::DestroyCtx(ResponseHdr::new(
             DpeErrorCode::NoError,
@@ -70,9 +74,13 @@ impl CommandExecution for DestroyCtxCmd {
 mod tests {
     use super::*;
     use crate::{
-        commands::{Command, CommandHdr},
-        dpe_instance::tests::SIMULATION_HANDLE,
+        commands::{Command, CommandHdr, InitCtxCmd},
+        context::ContextState,
+        dpe_instance::tests::{TestTypes, SIMULATION_HANDLE, TEST_HANDLE, TEST_LOCALITIES},
+        support::Support,
     };
+    use crypto::OpensslCrypto;
+    use platform::default::DefaultPlatform;
     use zerocopy::AsBytes;
 
     const TEST_DESTROY_CTX_CMD: DestroyCtxCmd = DestroyCtxCmd {
@@ -90,5 +98,145 @@ mod tests {
             Ok(Command::DestroyCtx(TEST_DESTROY_CTX_CMD)),
             Command::deserialize(&command)
         );
+    }
+
+    #[test]
+    fn test_destroy_context() {
+        let mut env = DpeEnv::<TestTypes> {
+            crypto: OpensslCrypto::new(),
+            platform: DefaultPlatform,
+        };
+        let mut dpe = DpeInstance::new(&mut env, Support::default()).unwrap();
+
+        InitCtxCmd::new_use_default()
+            .execute(&mut dpe, &mut env, TEST_LOCALITIES[0])
+            .unwrap();
+
+        // Wrong locality.
+        assert_eq!(
+            Err(DpeErrorCode::InvalidLocality),
+            DestroyCtxCmd {
+                handle: ContextHandle::default(),
+                flags: DestroyCtxFlags::empty(),
+            }
+            .execute(&mut dpe, &mut env, TEST_LOCALITIES[1])
+        );
+
+        // create two dummy contexts at indices 0 and 1, with 1 being the child of 0
+        activate_dummy_context(&mut dpe, 0, Context::ROOT_INDEX, &TEST_HANDLE, &[1]);
+        activate_dummy_context(&mut dpe, 1, 0, &ContextHandle::default(), &[]);
+        // destroy context[1]
+        assert_eq!(
+            Ok(Response::DestroyCtx(ResponseHdr::new(
+                DpeErrorCode::NoError,
+            ))),
+            DestroyCtxCmd {
+                handle: ContextHandle::default(),
+                flags: DestroyCtxFlags::empty(),
+            }
+            .execute(&mut dpe, &mut env, TEST_LOCALITIES[0])
+        );
+        assert_eq!(dpe.contexts[1].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[0].children, 0);
+        // destroy context[0]
+        assert_eq!(
+            Ok(Response::DestroyCtx(ResponseHdr::new(
+                DpeErrorCode::NoError,
+            ))),
+            DestroyCtxCmd {
+                handle: TEST_HANDLE,
+                flags: DestroyCtxFlags::empty(),
+            }
+            .execute(&mut dpe, &mut env, TEST_LOCALITIES[0])
+        );
+        assert_eq!(dpe.contexts[0].state, ContextState::Inactive);
+
+        activate_dummy_context(
+            &mut dpe,
+            0,
+            Context::ROOT_INDEX,
+            &ContextHandle::default(),
+            &[1, 2],
+        );
+        activate_dummy_context(
+            &mut dpe,
+            1,
+            0,
+            &ContextHandle([1; ContextHandle::SIZE]),
+            &[3, 4],
+        );
+        activate_dummy_context(
+            &mut dpe,
+            2,
+            0,
+            &ContextHandle([2; ContextHandle::SIZE]),
+            &[5, 6],
+        );
+        activate_dummy_context(
+            &mut dpe,
+            3,
+            1,
+            &ContextHandle([3; ContextHandle::SIZE]),
+            &[],
+        );
+        activate_dummy_context(
+            &mut dpe,
+            4,
+            1,
+            &ContextHandle([4; ContextHandle::SIZE]),
+            &[],
+        );
+        activate_dummy_context(
+            &mut dpe,
+            5,
+            2,
+            &ContextHandle([5; ContextHandle::SIZE]),
+            &[],
+        );
+        activate_dummy_context(
+            &mut dpe,
+            6,
+            2,
+            &ContextHandle([6; ContextHandle::SIZE]),
+            &[],
+        );
+
+        // destroy context[0] and all descendents
+        assert_eq!(
+            Ok(Response::DestroyCtx(ResponseHdr::new(
+                DpeErrorCode::NoError,
+            ))),
+            DestroyCtxCmd {
+                handle: ContextHandle::default(),
+                flags: DestroyCtxFlags::DESTROY_CHILDREN_FLAG_MASK,
+            }
+            .execute(&mut dpe, &mut env, TEST_LOCALITIES[0])
+        );
+        assert_eq!(dpe.contexts[0].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[1].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[2].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[3].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[4].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[5].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[6].state, ContextState::Inactive);
+        assert_eq!(dpe.contexts[0].children, 0);
+        assert_eq!(dpe.contexts[1].children, 0);
+        assert_eq!(dpe.contexts[2].children, 0);
+        assert_eq!(dpe.contexts[3].children, 0);
+    }
+
+    fn activate_dummy_context(
+        dpe: &mut DpeInstance,
+        idx: usize,
+        parent_idx: u8,
+        handle: &ContextHandle,
+        children: &[u8],
+    ) -> () {
+        dpe.contexts[idx].state = ContextState::Active;
+        dpe.contexts[idx].handle = *handle;
+        dpe.contexts[idx].parent_idx = parent_idx;
+        for i in children {
+            dpe.contexts[idx].add_child(*i as usize).unwrap();
+        }
     }
 }
