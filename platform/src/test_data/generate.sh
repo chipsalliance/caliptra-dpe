a5c0d3bbcb0f1645bc644619b10a2f58f3606a21
Carl Lundin <clundin@google.com>
2024-10-11 14:19:21 -0700

    Make certs expire in 1000 years

--- platform/src/test_data/generate.sh
+++ platform/src/test_data/generate.sh
@@ -4,14 +4,14 @@
 set -ex
 
 openssl ecparam -name prime256v1 -genkey -noout -out key_256.pem
-openssl req -new -key key_256.pem -x509 -nodes -days 365 -out cert_256.pem \
+openssl req -new -key key_256.pem -x509 -nodes -days 365000 -out cert_256.pem \
   -addext keyUsage=critical,keyCertSign \
   -subj /CN="DPE Test Alias"/
 openssl ec -in key_256.pem -outform DER -out key_256.der
 openssl x509 -in cert_256.pem -outform DER -out cert_256.der
 
 openssl ecparam -name secp384r1 -genkey -noout -out key_384.pem
-openssl req -new -key key_384.pem -x509 -nodes -days 365 -out cert_384.pem \
+openssl req -new -key key_384.pem -x509 -nodes -days 365000 -out cert_384.pem \
   -addext keyUsage=critical,keyCertSign \
   -subj /CN="DPE Test Alias"/
 openssl ec -in key_384.pem -outform DER -out key_384.der
