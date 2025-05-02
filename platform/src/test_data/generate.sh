11784a968c35b339f639627e4af539f544b98877
Carl Lundin <clundin@google.com>
2025-05-02 16:11:37 -0700

    crypto: Add ML-DSA87 test artifacts

--- platform/src/test_data/generate.sh
+++ platform/src/test_data/generate.sh
@@ -16,3 +16,14 @@ openssl req -new -key key_384.pem -x509 -nodes -days 365000 -out cert_384.pem \
   -subj /CN="DPE Test Alias"/
 openssl ec -in key_384.pem -outform DER -out key_384.der
 openssl x509 -in cert_384.pem -outform DER -out cert_384.der
+
+# Note: Requires OpenSSL 3.5+
+
+# We only output the seed so the PEM file can be de-serialized by Rust Crypto's pkcs8 crate.
+# You can construct everything you need from just the seed.
+openssl genpkey -algorithm ML-DSA-87 -provparam ml-dsa.output_formats=bare-seed -out key_mldsa_87.pem
+openssl req -new -key key_mldsa_87.pem -x509 -nodes -days 365000 -out cert_mldsa_87.pem \
+  -addext keyUsage=critical,keyCertSign \
+  -subj /CN="DPE Test Alias"/
+openssl pkey -in key_mldsa_87.pem -outform DER -out key_mldsa_87.der
+openssl x509 -in cert_mldsa_87.pem -outform DER -out cert_mldsa_87.der
