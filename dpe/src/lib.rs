e09a6f994c6adee9147ef889f1660d3dae8c67b4
Zach Halvorsen <zhalvorsen@google.com>
2026-01-29 00:28:25 +0000

    Reduce max cert size
    This is to enable minimal ML-DSA functionality until chunked certs are supported.

--- dpe/src/lib.rs
+++ dpe/src/lib.rs
@@ -41,7 +41,7 @@ pub use crypto::{ecdsa::EcdsaAlgorithm, ExportedCdiHandle, MAX_EXPORTED_CDI_SIZE
 
 // Max cert size returned by CertifyKey
 #[cfg(feature = "ml-dsa")]
-const MAX_CERT_SIZE: usize = 17 * 1024;
+const MAX_CERT_SIZE: usize = 12 * 1024 + 256;
 #[cfg(not(feature = "ml-dsa"))]
 const MAX_CERT_SIZE: usize = 7872;
 #[cfg(not(feature = "arbitrary_max_handles"))]
