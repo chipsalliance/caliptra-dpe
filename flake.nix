a638efdecb8f89cf8c50d2cbc091f557024ab201
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2026-04-03 13:40:11 -0700

    Migrate to xtask flow and add a nix flake (#548)
    * Port scripts and CI to xtask flow
    
    This removes some shell scripts and let's us use Rust for writing
    automation and scripts.
    
    This pattern matches caliptra-sw and caliptra-mcu-sw.
    
    * Add nix developer flake, update CI (apt install nix), and document environment

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Development environment for caliptra-dpe";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustup
+            go
+            golint
+            uv
+            openssl
+            pkg-config
+          ];
+
+          shellHook = ''
+            # Ensure the toolchains are installed
+            rustup toolchain install $(grep channel rust-toolchain.toml | cut -d'"' -f2)
+            rustup toolchain install nightly-2025-07-08
+          '';
+        };
+      }
+    );
+}
