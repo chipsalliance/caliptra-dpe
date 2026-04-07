b022e077da6e1e336eb0c6211e94c0e9df2791a8
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2026-04-07 07:34:50 -0700

    Generate test artifacts at build time via crate (#550)
    This ensures we are constantly exercising the artifact generation and
    cleans the repo up a bit.

---
+++ test-artifacts/src/lib.rs
@@ -0,0 +1,46 @@
+// Licensed under the Apache-2.0 license
+
+#![no_std]
+
+macro_rules! include_artifacts {
+    ($cert_pem:ident, $cert_der:ident, $key_pem:ident, $key_der:ident, $suffix:expr) => {
+        pub const $cert_pem: &str = include_str!(concat!(
+            env!("OUT_DIR"),
+            "/test_data/cert_",
+            $suffix,
+            ".pem"
+        ));
+        pub const $cert_der: &[u8] = include_bytes!(concat!(
+            env!("OUT_DIR"),
+            "/test_data/cert_",
+            $suffix,
+            ".der"
+        ));
+        pub const $key_pem: &str =
+            include_str!(concat!(env!("OUT_DIR"), "/test_data/key_", $suffix, ".pem"));
+        pub const $key_der: &[u8] =
+            include_bytes!(concat!(env!("OUT_DIR"), "/test_data/key_", $suffix, ".der"));
+    };
+}
+
+include_artifacts!(
+    CERT_P256_PEM,
+    CERT_P256_DER,
+    KEY_P256_PEM,
+    KEY_P256_DER,
+    "256"
+);
+include_artifacts!(
+    CERT_P384_PEM,
+    CERT_P384_DER,
+    KEY_P384_PEM,
+    KEY_P384_DER,
+    "384"
+);
+include_artifacts!(
+    CERT_MLDSA_87_PEM,
+    CERT_MLDSA_87_DER,
+    KEY_MLDSA_87_PEM,
+    KEY_MLDSA_87_DER,
+    "mldsa_87"
+);
