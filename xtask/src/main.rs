1ff4b13274fa74d330f57e3d26058bd2b5e01119
leongross <leon.gross@9elements.com>
2026-05-06 16:13:29 +0200

    ci: xtask, nightly
    - extend xtask to use required `cargo +nightly
    - change `caliptra-cfi` cargo source to fork with miri patches

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -395,8 +395,8 @@ fn cargo_miri(
     miri_args: &MiriArgs,
 ) -> Result<()> {
     let mut cmd = match miri_args.nextest {
-        true => cargo().args(["miri", "nextest", "run"]),
-        false => cargo().args(["miri", "test"]),
+        true => cargo().nightly().args(["miri", "nextest", "run"]),
+        false => cargo().nightly().args(["miri", "test"]),
     };
 
     if miri_args.nextest {
@@ -524,6 +524,10 @@ impl Cmd {
         self.0
             .output()
             .map_err(|e| anyhow!("Failed to execute {:?}: {}", self.0, e))
+    }
+    fn nightly(mut self) -> Self {
+        self.0.arg("+nightly");
+        self
     }
 }
 
